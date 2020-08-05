d0e26b1cdfcc6a91b48a5bbce9de5767a9cbc541
Kevin Lacker <lacker@gmail.com>
2020-08-05 15:36:48 -0700

    deck validation partly useful

--- logs.py
+++ logs.py
@@ -86,6 +86,16 @@ def xobj(cls, subparsers):
     return find_answer
 
 
+class CardChoice(object):
+    def __init__(self):
+        pass
+
+CardChoice.parser = xobj(CardChoice, {
+    "not_picked": xlist(xstr),
+    "picked": xstr,
+    "floor": xint,
+})
+
 class GameLog(object):
     parser = None
     
@@ -103,9 +113,14 @@ class GameLog(object):
         if not GameLog.parser:
             GameLog.parser = xobj(GameLog, {
                 "ascension_level": xint,
+                "card_choices": xlist(CardChoice.parser),
+                "character_chosen": xstr,
                 "chose_seed": xbool,
+                "floor_reached": xint,
                 "is_daily": xbool,
                 "is_endless": xbool,
+                "items_purchased": xlist(xstr),
+                "item_purchase_floors": xlist(xint),
                 "master_deck": xlist(xstr),
             })
         
@@ -148,7 +163,40 @@ class GameLog(object):
         print(f"run {self.name}:")
         print(json.dumps(self.data, indent=2))
 
+    def initial_deck(self):
+        if self.character_chosen == "IRONCLAD":
+            return ["Strike_R"] * 5 + ["Defend_R"] * 4 + ["Bash", "AscendersBane"]
+        elif self.character_chosen == "SILENT":
+            return ["Strike_G"] * 5 + ["Defend_G"] * 5 + ["Neutralize", "Survivor", "AscendersBane"]
+        elif self.character_chosen == "DEFECT":
+            return ["Strike_B"] * 4 + ["Defend_B"] * 4 + ["Dualcast", "Zap", "AscendersBane"]
+        elif self.character_chosen == "WATCHER":
+            return ["Strike_P"] * 4 + ["Defend_P"] * 4 + ["Eruption", "Vigilance", "AscendersBane"]
+        else:
+            raise ValueError(f"character_chosen is {self.character_chosen}")
+        return answer
         
+    def validate_deck(self):
+        deck = self.initial_deck()
+        for floor in range(1, self.floor_reached):
+            for choice in self.card_choices:
+                if choice.floor == floor:
+                    deck.append(choice.picked)
+            for f, item in zip(self.item_purchase_floors, self.items_purchased):
+                if floor == f:
+                    deck.append(item)
+
+        master = sorted(self.master_deck)
+        repro = sorted(deck)
+        if master == repro:
+            return True
+        
+        self.show()
+        print(f"reproduced deck: {repro}")
+        print(f"actual deck: {master}")
+        return False
+        
+    
     
 def iter_logs():
     for month in MONTHS:
@@ -198,15 +246,13 @@ def save_good_games_locally():
 
     
 if __name__ == "__main__":
-    num = 0
-    den = 0
+    validated = 0
     for game in iter_local():
-        den += 1
-        if game.is_good():
-            num += 1
-        if den % 1000 == 0:
-            print(f"{num}/{den} = {num/den} are good")
-        if den == 107610:
-            game.show()
-    print(f"{num}/{den} = {num/den} are good")
+        if not game.validate_deck():
+            print(f"validated {validated} decks")
+            break
+        validated += 1
+        if den % 10 == 0:
+            print(f"validated {validated}")
+
 
