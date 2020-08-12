056e2bb6c50141390664fcec2827c5d3d8e320e1
Kevin Lacker <lacker@gmail.com>
2020-08-12 12:08:11 -0700

    stuff

--- mod.py
+++ mod.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
-if __name__ == "__main__":
-    print("hello world")
+import os
+import sys
 
+DIR = os.path.dirname(__file__)
+
+if __name__ == "__main__":
+    logfile = os.path.join(DIR, "tmp", "mod.log")
+    log = open(logfile, "w")
+    print("ready to log stuff", file=log)
+    print("Ready")
+    for line in sys.stdin:
+        print(f"received: {line}", file=log, flush=True)
+        print("WAIT 100")
