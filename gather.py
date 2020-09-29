323cd3ddaa232dad1b1137ae72a0558ce2276b3b
Kevin Lacker <lacker@gmail.com>
2020-09-28 20:03:14 -0700

    data-gathering

--- gather.py
+++ gather.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
-
 # The data-gathering phase.
+
+import os
+import random
+
 from connection import Connection
-from spire_env import SpireEnv
+
+RUNLOGDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "runlogs")
 
 
-def gather(run_name, seed):
-    conn = Connection()
-    env = SpireEnv(conn)
+def logname(run_id, seed):
+    return os.path.join(RUNLOGDIR, f"run{run_id}-{seed}.jlog")
+
+
+def gather(conn, run_id, seed):
+    random.seed(seed)
+    status = conn.get_status()
+    if status.has_game():
+        raise ValueError("gather must be called when there is no active game")
+    conn.start_game(seed=seed)
+    while conn.get_status().has_game():
+        conn.make_random_move()
+    f = open(logname(run_id, seed), "w")
+    for line in conn.log:
+        f.write(line + "\n")
+    f.close()
 
 
 if __name__ == "__main__":
+    conn = Connection()
     for seed in range(1000):
-        pass
+        gather(conn, 1, seed)
